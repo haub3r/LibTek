caa0653091a09ba715d7b16e4157df7c45cc2c7d
Iiro <iirovanninen1@gmail.com>
2014-09-14 11:23:10 +0300

    Version 1.1
    Added CSS. Default bootstrap CSS overridden by style.css @ /css.

--- public/controllers/MainCtrl.js
+++ public/controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 // controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'To the moon and back!';	
+	$scope.tagline = 'Home page';	
 
 });
